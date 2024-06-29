2dfb3b60131abbee589a5f24542f4b2901ea2864
katin1522 <20220026@ricaldone.edu.sv>
2024-06-29 17:46:01 -0600

    Trabajo

--- app/src/main/java/katherine/ceron/ptc/MainActivity_Menu.kt
+++ app/src/main/java/katherine/ceron/ptc/MainActivity_Menu.kt
@@ -11,7 +11,7 @@ class MainActivity_Menu : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main_menu)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.menu)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
--- app/src/main/java/katherine/ceron/ptc/MainActivity_recuperarContrasena.kt
+++ app/src/main/java/katherine/ceron/ptc/MainActivity_recuperarContrasena.kt
@@ -11,7 +11,7 @@ class MainActivity_recuperarContrasena : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main_recuperar_contrasena)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.recuperar_contra)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
