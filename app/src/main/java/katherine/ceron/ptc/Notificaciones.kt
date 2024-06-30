d1f73f540dcb07c3c4b1c6fd2a0c6ec24bbf11ea
fabiola <20230033@ricaldone.edu.sv>
2024-06-29 20:04:14 -0600

    trabajo 2

---
+++ app/src/main/java/katherine/ceron/ptc/Notificaciones.kt
@@ -0,0 +1,20 @@
+package katherine.ceron.ptc
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Notificaciones : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_notificaciones)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.notificaciones)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
