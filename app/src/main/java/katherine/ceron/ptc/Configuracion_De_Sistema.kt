1766abc5cf80697e6728422fe0dcc389a1eb245a
fabiola <20230033@ricaldone.edu.sv>
2024-07-01 22:21:44 -0600

    agregue 2 pantallas

---
+++ app/src/main/java/katherine/ceron/ptc/Configuracion_De_Sistema.kt
@@ -0,0 +1,20 @@
+package katherine.ceron.ptc
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Configuracion_De_Sistema : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_configuracion_de_sistema)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
